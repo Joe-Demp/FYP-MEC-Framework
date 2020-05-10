ec142ece3c225869d871d19a60e05f2a15d21442
DarraghClarke <darragh.clarke@ucdconnect.ie>
2020-05-10 18:31:46 +0100

    modified docker
    Former-commit-id: 997943fa9d4f1e778d7ed52c49c9701a29a83c45

--- DockerImage/src/main/java/service/docker/Main.java
+++ DockerImage/src/main/java/service/docker/Main.java
@@ -1,10 +1,21 @@
 package service.docker;
 
+import java.util.Scanner;
+
 public class Main {
 
+    /**
+     * This file simply served as a testing base for a docker image, which saved time on transporting dockerfiles that are multiple GB in size,
+     * it simply prints when it launches and then can
+     * @param args
+     */
     public static void main(String[] args) {
-        for(int i=0;i<100;i++){
-            System.out.println("Wide Awaaaaake");
+        Scanner inputScanner = new Scanner(System.in);
+
+        System.out.println("Sample Service launched, ready for input.....");
+        String input = inputScanner.nextLine();
+        while(!input.isEmpty())  {
+            System.out.println("Input recieved " +input);
         }
     }
 }
