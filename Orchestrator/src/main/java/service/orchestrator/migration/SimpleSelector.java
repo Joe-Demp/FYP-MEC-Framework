fc63fbb082e1898deee25ffa9950ff8e8b76265f
Joseph Dempsey <joseph.dempsey@ucdconnect.ie>
2021-04-12 01:37:32 +0100

    Changed SimpleSelector to select a random node instead of relying on Stream.findAny()
    Signed-off-by: Joseph Dempsey <joseph.dempsey@ucdconnect.ie>
    
    Former-commit-id: f8703888a538b9c8fb8856690f69f68df3c5af5e

--- Orchestrator/src/main/java/service/orchestrator/migration/SimpleSelector.java
+++ Orchestrator/src/main/java/service/orchestrator/migration/SimpleSelector.java
@@ -3,16 +3,21 @@ package service.orchestrator.migration;
 import service.orchestrator.clients.MobileClient;
 import service.orchestrator.nodes.ServiceNode;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Random;
 
 /**
  * Selector that chooses any old ServiceNode.
  */
 public class SimpleSelector implements Selector {
+    private static Random random = new Random(2021);
+
     @Override
     public ServiceNode select(Collection<ServiceNode> nodes, MobileClient mobileClient) {
-        return nodes.stream()
-                .findAny()
-                .orElse(null);
+        if (nodes.isEmpty()) return null;
+
+        int index = random.nextInt(nodes.size());
+        return new ArrayList<>(nodes).get(index);
     }
 }
