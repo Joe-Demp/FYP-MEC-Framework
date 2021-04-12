8f51f0ace38c7ae9a625a516d210fd0680e91a1c
Joseph Dempsey <joseph.dempsey@ucdconnect.ie>
2021-04-12 04:03:42 +0100

    Changed some ports and wrote a todo.
    Signed-off-by: Joseph Dempsey <joseph.dempsey@ucdconnect.ie>
    
    Former-commit-id: 5f7049d542cd1a007b4cc3e12a62bbbf9dc5c6a3

--- Core/src/main/java/service/core/Constants.java
+++ Core/src/main/java/service/core/Constants.java
@@ -1,9 +1,13 @@
 package service.core;
 
 public interface Constants {
-	int MOBILE_PING_SERVER_PORT = 8092;
+	int MOBILE_PING_SERVER_PORT = 8071;
 
 	// Note: this has to be constant to get past firewalls
+	/*
+	 * todo note there is an issue coming down the line: Robert-PC has ports 8090-8100 exposed.
+	 *  Need TransferServer on port 8095!
+	 * */
 	int TRANSFER_SERVER_PORT = 8085;
 
 	int SERVICE_PORT = 8090;
